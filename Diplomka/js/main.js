340edc7ebd8f9ffe672d8560778bc03c55d482a2
S1onny <100217381+S1onny@users.noreply.github.com>
2024-08-22 15:02:21 +0400

    add update

--- Diplomka/js/main.js
+++ Diplomka/js/main.js
@@ -1,4 +1,21 @@
+document.addEventListener('click', burgerInit)
 
+function burgerInit(e) {
+
+    const burgerIcon = e.target.closest ('.burger-icon')
+    const burgerNavLink = e.target.closest ('.nav__link')
+
+    if (!burgerIcon && !burgerNavLink) return
+
+    if(document.documentElement.clientWidth > 900) return
+
+    if (!document.body.classList.contains('body--opened-menu')) {
+        document.body.classList.add('body--opened-menu')
+    } else {
+        document.body.classList.remove('body--opened-menu')
+    }
+
+}
 
 // ============== Верхний мейн слайдер
 const previewSlider = new Swiper('.slider-preview', {
